d4b3170041b8d52d38c2cd236c690c7da5b001b3
LeepyShah <158757009+LeepyShah@users.noreply.github.com>
2025-04-15 10:50:44 +0530

    Message

--- Day9/script.js
+++ Day9/script.js
@@ -1,45 +1,36 @@
 const circle1 = document.getElementById("circle1");
 const circle2 = document.getElementById("circle2");
-circle1.addEventListener("drag", (event) => {
-  console.log("dragging");
+circle2.addEventListener("dragover", (e) => e.preventDefault());
+circle2.addEventListener("drop", (e) => {
+  e.preventDefault();
+  // Get smallest nested size
+  let latest = circle2.lastElementChild;
+  // If there are no nested circles, set size to 80% of the circle2 size
+  let size = latest
+    ? parseFloat(latest.style.width) * 0.8
+    : circle2.offsetWidth * 0.8;
+  if (size < 5) return;
+  const originalColor = window.getComputedStyle(circle1).backgroundColor;
+  // Create new nested circle
+  const nested = document.createElement("div");
+  nested.classList.add("nested");
+  nested.style.width = size + "px";
+  nested.style.height = size + "px";
+  nested.style.backgroundColor = originalColor;
+  nested.style.borderRadius = "50%";
+  nested.style.position = "absolute";
+  // Center it inside previous one
+  nested.style.left = (circle2.offsetWidth - size) / 2 + "px";
+  nested.style.top = (circle2.offsetHeight - size) / 2 + "px";
+  circle2.appendChild(nested);
+  // Change the color of the original circle
+  circle1.style.backgroundColor = getRandomColor();
 });
-circle2.addEventListener(
-  "dragover",
-  (event) => {
-    event.preventDefault(); // prevent default to allow drop
-  },
-  false
-);
-circle2.addEventListener("drop", (event) => {
-  circle1.style.backgroundColor = getColor();
-  size = circle1.offsetWidth;
-  new_circle = document.createElement("div");
-//   new_circle.classList.add("circle2");
-  new_circle.style.width = size * 0.8 + "px";
-  new_circle.style.height = size * 0.8 + "px";
-  console.log(new_circle.style.width);
-  console.log(new_circle.style.height);
-  new_circle.style.backgroundColor = getColor();
-  new_circle.style.position = "absolute";
-  new_circle.style.borderRadius = "50%";
-  new_circle.style.justifyContent = "space-between";
-  new_circle.style.top =
-    Math.floor(Math.random() * (circle2.offsetHeight - size * 0.8)) + "px";
-  new_circle.style.left =
-    Math.floor(Math.random() * (circle2.offsetWidth - size * 0.8)) + "px";
-  new_circle.setAttribute("draggable", true);
-  new_circle.addEventListener("drag", (event) => {
-    console.log("dragging");
-  });
-  circle2.appendChild(new_circle);
- 
-});
-function getColor() {
+function getRandomColor() {
   const letters = "0123456789ABCDEF";
   let color = "#";
   for (let i = 0; i < 6; i++) {
     color += letters[Math.floor(Math.random() * 16)];
-    console.log(color);
   }
   return color;
 }
